11677a30ec892017137a1feb17a0ee726c2dcba4
Arafath <arafathctk@gmail.com>
2024-01-13 22:47:23 +0530

    covered promises

--- promise.js
+++ promise.js
@@ -1,23 +1,26 @@
-const p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("okeyy");
-    }, 2000);
-  });
-  const p2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("okeyy2");
-    }, 4000);
-  });
-  
-  async function pResolve() {
-    console.log("Worlddd!!!");
-    const x = await p;
-    console.log("Hellooooo!!!!");
-    console.log(x);
-  
-    const y = await p2;
-    console.log("Hiiii");
-    console.log(y);
-  }
-  
-  pResolve();
+let myPromise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("p1 resolved");
+  }, 2000);
+});
+
+let myPromise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("p2 resolved");
+  }, 4000);
+});
+
+async function promiseExecution() {
+  console.log("started");
+  let p1 = await myPromise1;
+  console.log("got the p1");
+  console.log(p1);
+  console.log("p1 printed");
+
+  let p2 = myPromise2;
+  console.log("got the p2");
+  console.log(p2);
+  console.log("p2 printed");
+}
+
+promiseExecution();
